86567b830f2d96dca2d08cf0005839c8c6723f6a
Ankit Gupta <ankitkumargupta00@gmail.com>
2020-11-21 20:48:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import List from "./components/List";
+import friends from "./friends";
 function App() {
+  const formatDate = () => {
+    let date = new Date();
+    return `${date.getDate()}-${date.getMonth()}`;
+  };
+  let todayBirth = [];
+  let todays = () => {
+    let date = new Date();
+    let formatDate = `${date.getDate()}-${date.getMonth()}`;
+    friends.map((friend) => {
+      let dob = friend.dob.split("-");
+      let obj = {};
+      if (formatDate === `${dob[0]}-${dob[1]}`) {
+        obj["id"] = friend.id;
+        obj["name"] = friend.name;
+        obj["age"] = friend.age;
+        obj["dob"] = friend.dob;
+        obj["image"] = friend.image;
+        return todayBirth.push(obj);
+      }
+    });
+  };
+  todays();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{todayBirth.length} birthdays today</h3>
+        <List data={todayBirth} />
+      </section>
+    </main>
   );
 }
 
